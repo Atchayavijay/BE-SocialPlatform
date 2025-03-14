931fb65d28202942cf004209bd827d766b5ee041
Atchayavijay <atchuvijay83@gmail.com>
2025-03-14 11:03:46 +0530

    url

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ connectToDb();
 dotenv.config();
 
 
+// cloudinary.v2.config({
+//   cloud_name: process.env.Cloud_Name,
+//   api_key: process.env.Cloud_Api,
+//   api_secret: process.env.Cloud_Secret,
+// });
+
 cloudinary.v2.config({
-  cloud_name: process.env.Cloud_Name,
-  api_key: process.env.Cloud_Api,
-  api_secret: process.env.Cloud_Secret,
-});
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,  
+  api_key: process.env.CLOUDINARY_API_KEY,        
+  api_secret: process.env.CLOUDINARY_API_SECRET,  
+})
 
 
 app.use(express.json());
